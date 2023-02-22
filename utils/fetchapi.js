a2f19095950837cb3f52fb004abd32cae5368993
Grayson-Remington <graysonbremington@gmail.com>
2023-02-22 10:26:20 -0500

    hide the key

--- utils/fetchapi.js
+++ utils/fetchapi.js
@@ -5,8 +5,7 @@ export const baseUrl = 'https://bayut.p.rapidapi.com';
 export const fetchApi = async (url) => {
 	const { data } = await axios.get(url, {
 		headers: {
-			'X-RapidAPI-Key':
-				'266fd1f939mshc50e936c1430855p133d4fjsn65367d843745',
+			'X-RapidAPI-Key': `${process.env.REACT_APP_API_KEY}`,
 			'X-RapidAPI-Host': 'bayut.p.rapidapi.com',
 		},
 	});
